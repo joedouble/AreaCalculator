3c0d7b88e9495604b8a92806645829eaa2ab2b35
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-22 14:30:19 -0500

    Committing working code.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,4 +1,29 @@
 package com.company;
 
 public class Calculator {
+
+    private int width;
+    private int length;
+
+    public void setWidth(int calculatorWidth) {
+        width = calculatorWidth;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setLength(int calculatorLength) {
+        length = calculatorLength;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int calculateArea(int width, int length) {
+        int area = width * length;
+        return area;
+    }
+
 }
--- src/com/company/CalculatorDemo.java
+++ src/com/company/CalculatorDemo.java
@@ -3,6 +3,12 @@ package com.company;
 public class CalculatorDemo {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Calculator rectangleArea = new Calculator();
+
+        int recArea = rectangleArea.calculateArea(3,7);
+
+        System.out.println(recArea);
+
     }
 }
